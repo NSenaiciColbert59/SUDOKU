fc0282d0f17230a95589cbe3b80fa851a9e61280
NSenaiciColbert59 <94219001+NSenaiciColbert59@users.noreply.github.com>
2021-11-19 09:39:18 +0100

    Update stack.py

--- src/prof/j3/stack.py
+++ src/prof/j3/stack.py
@@ -11,7 +11,8 @@ class Stack:
             >>> p.top == None
             True
         """
-        pass
+        self.stack = []
+        self.top = None
         
     def push(self, el):
         """
@@ -28,8 +29,9 @@ class Stack:
             >>> p.top == (12, 1, 9)
             True
         """
-        pass
-    
+        self.stack.append(el)
+        self.top = el
+
     def empty(self):
         """
             Vérifie si la pile est vide
@@ -42,7 +44,8 @@ class Stack:
             >>> p.empty()
             False
         """
-        pass
+        return len(self.stack) == 0
+        
         
     def pop(self):
         """
@@ -68,7 +71,15 @@ class Stack:
             >>> p.pop()
             13
         """
-        pass
+        if not self.empty():
+            el = self.stack.pop()
+            if not self.empty():
+                self.top = self.stack[-1]
+            else:
+                self.top = None
+            return el
+        return None
+            
         
 if __name__ == '__main__':
     import doctest
